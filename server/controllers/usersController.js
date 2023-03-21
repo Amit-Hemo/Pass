8f299ca7d7d6c3d177105ef51c712180cacc9018
Nadav Buchwalter <ndbuchwalter94@gmail.com>
2023-03-21 20:37:28 +0200

    Added updatePassword function

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -99,11 +99,29 @@ async function handleRefreshToken(req, res) {
   }
 }
 
-//TODO:  pick problem - doesnt really do required , check what about the req.user use
+async function updatePassword(req, res) {
+  const { password, newPassword } = req.body;
+  const { uuid } = req.params;
+
+  try {
+    const user = await UserModel.findOne({ uuid });
+    if (!user) return res.status(404).json({ error: "User not found" });
+    const isValidPassword = await user.comparePassword(password);
+    if (!isValidPassword)
+      return res.status(400).json({ error: "Invalid password" });
+    user.password = newPassword;
+    await user.save();
+    return res.status(200).json({ messege: "Password updated successfully" });
+  } catch (error) {
+    return res.status(409).json({ error: "Server error" });
+  }
+}
+
 module.exports = {
   createUser,
   updateUser,
   loginUser,
   handleRefreshToken,
   logoutUser,
+  updatePassword,
 };
