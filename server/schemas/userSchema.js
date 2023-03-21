c28c586df1aebc63ce6a02ccb09f6823b450437d
Nadav Buchwalter <ndbuchwalter94@gmail.com>
2023-03-21 12:53:34 +0200

    Changed schemas to be stricted schemas

--- server/schemas/userSchema.js
+++ server/schemas/userSchema.js
@@ -1,36 +1,37 @@
 const z = require("zod");
 
-const userSchema = z.object({
-  firstName: z.string().min(2).max(100).trim(),
-  lastName: z.string().min(2).max(100).trim(),
-  email: z.string().trim().email(),
-  password: z
-    .string()
-    .min(8)
-    .max(100)
-    .refine(
-      (value) => {
-        const uppercaseRegex = /[A-Z]/;
-        const lowercaseRegex = /[a-z]/;
-        const digitRegex = /\d/;
-        const specialCharRegex = /[!@#$%^&*]/;
+const userSchema = z
+  .object({
+    firstName: z.string().min(2).max(100).trim(),
+    lastName: z.string().min(2).max(100).trim(),
+    email: z.string().trim().email(),
+    password: z
+      .string()
+      .min(8)
+      .max(100)
+      .refine(
+        (value) => {
+          const uppercaseRegex = /[A-Z]/;
+          const lowercaseRegex = /[a-z]/;
+          const digitRegex = /\d/;
+          const specialCharRegex = /[!@#$%^&*]/;
 
-        return (
-          uppercaseRegex.test(value) &&
-          lowercaseRegex.test(value) &&
-          digitRegex.test(value) &&
-          specialCharRegex.test(value)
-        );
-      },
-      {
-        message:
-          "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character",
-        path: ["password"],
-      }
-    ),
-  confirmPassword: z.string().min(8),
-  braintreeCustomerId: z.string().nonempty().optional(),
-  refreshToken: z.string().nonempty().optional()
-});
+          return (
+            uppercaseRegex.test(value) &&
+            lowercaseRegex.test(value) &&
+            digitRegex.test(value) &&
+            specialCharRegex.test(value)
+          );
+        },
+        {
+          message:
+            "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character",
+          path: ["password"],
+        }
+      ),
+    confirmPassword: z.string().min(8),
+    braintreeCustomerId: z.string().nonempty().optional(),
+  })
+  .strict();
 
 module.exports = userSchema;
--- server/schemas/uuidSchema.js
+++ server/schemas/uuidSchema.js
@@ -1,10 +1,12 @@
-const z = require('zod');
-const { validate: uuidValidate, version: uuidVersion } = require('uuid');
+const z = require("zod");
+const { validate: uuidValidate, version: uuidVersion } = require("uuid");
 
-const uuidSchema = z.object({
-  uuid: z
-    .string()
-    .refine((value) => uuidValidate(value) && uuidVersion(value) === 4),
-});
+const uuidSchema = z
+  .object({
+    uuid: z
+      .string()
+      .refine((value) => uuidValidate(value) && uuidVersion(value) === 4),
+  })
+  .strict();
 
 module.exports = uuidSchema;
