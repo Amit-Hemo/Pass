a10a6c670ef28ab44f083c3411d8b8e70b7cee23
Nadav Buchwalter <ndbuchwalter94@gmail.com>
2023-04-19 13:01:46 +0300

    Deleted parseToIL util function

--- server/utils/parseToIL.js
+++
@@ -1,7 +0,0 @@
-function parseToIL(utcString){
-    const utcTime = new Date(utcString);
-    const date = utcTime.toLocaleDateString("he-IL");
-    const time = utcTime.toLocaleTimeString("he-IL");
-    return {date,time}
-}
-module.exports =parseToIL;
