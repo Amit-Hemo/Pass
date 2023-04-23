b31474d7a222da063925aec21a7e6dc6cbee6255
Amit Hemo <itsamit442@gmail.com>
2023-04-23 13:22:56 +0300

    Edited middleware errors

--- server/middlewares/verifyAccessToken.js
+++ server/middlewares/verifyAccessToken.js
@@ -1,20 +1,26 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 function verifyAccessToken(req, res, next) {
   const authorizationHeader = req.headers.authorization;
-  if (!authorizationHeader?.startsWith("Bearer"))
-    return res.status(401).json({ error: "Authorization header not exist" });
+  if (!authorizationHeader?.startsWith('Bearer'))
+    return res.status(401).json({ error: 'Authorization header not exist' });
 
-  const accessToken = authorizationHeader.split(" ")[1];
+  const accessToken = authorizationHeader.split(' ')[1];
   if (!accessToken)
-    return res.status(403).json({ error: "Access token not exist" });
+    return res.status(403).json({ error: 'Access token not exist' });
 
   try {
     const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
     req.user = decoded.uuid;
     next();
   } catch (error) {
-    return res.status(403).json({ error: "Invalid access token" });
+    if (error.name === 'TokenExpiredError') {
+      return res.status(403).send({ error: 'Token is expired' });
+    } else if (error.name === 'JsonWebTokenError') {
+      return res.status(403).send({ error: 'Invalid Token' });
+    } else {
+      return res.status(500).send({ error: 'Server error' });
+    }
   }
 }
 
