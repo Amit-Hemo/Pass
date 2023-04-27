cd41d6529774232b2ec0b3480d103f72c9b05441
Nadav Buchwalter <ndbuchwalter94@gmail.com>
2023-04-27 14:04:33 +0300

    Add loading feature (ActivityIndicator) to PopUp

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import { Image, Platform, Text, TouchableOpacity, View } from 'react-native';
-import Modal from 'react-native-modal';
+import React from "react";
+import {
+  Image,
+  Platform,
+  Text,
+  TouchableOpacity,
+  View,
+  ActivityIndicator,
+} from "react-native";
+import Modal from "react-native-modal";
 
 const Popup = ({
   visible,
@@ -8,36 +15,47 @@ const Popup = ({
   isError,
   onClose = () => {},
   message,
+  isLoading = false,
 }) => {
   return (
     <Modal
       isVisible={visible}
-      animationIn={'bounceIn'}
-      animationOut={'bounceOutDown'}
-      className='w-2/3 self-center'
+      animationIn={"bounceIn"}
+      animationOut={"bounceOutDown"}
+      className="w-2/3 self-center"
     >
-      <View className='bg-white py-16 px-5 rounded-3xl border border-gray-300 shadow-md justify-center items-center'>
-        <Image
-          source={
-            isError
-              ? require('../../assets/fail-mark.png')
-              : require('../../assets/check-mark.png')
-          }
-          className='w-28 h-28 mb-8'
-        />
-        <Text className='text-xl text-center font-bold mb-4'>{message}</Text>
-
-        <TouchableOpacity
-          className={`border-2 border-blue-500 rounded-full px-4 pt-2 ${
-            Platform.OS === 'ios' ? 'pb-1' : 'pb-2'
-          } items-center justify-center`}
-          onPress={() => {
-            onClose();
-            setVisible(false);
-          }}
-        >
-          <Text className='text-blue-500 font-bold text-lg'>אישור</Text>
-        </TouchableOpacity>
+      <View className="bg-white py-16 px-5 rounded-3xl border border-gray-300 shadow-md justify-center items-center">
+        {isLoading ? (
+          <View>
+            <Text className="text-xl text-center font-bold mb-8">יש להמתין...</Text>
+            <ActivityIndicator size="large" />
+          </View>
+        ) : (
+          <>
+            <Image
+              source={
+                isError
+                  ? require("../../assets/fail-mark.png")
+                  : require("../../assets/check-mark.png")
+              }
+              className="w-28 h-28 mb-8"
+            />
+            <Text className="text-xl text-center font-bold mb-4">
+              {message}
+            </Text>
+            <TouchableOpacity
+              className={`border-2 border-blue-500 rounded-full px-4 pt-2 ${
+                Platform.OS === "ios" ? "pb-1" : "pb-2"
+              } items-center justify-center`}
+              onPress={() => {
+                onClose();
+                setVisible(false);
+              }}
+            >
+              <Text className="text-blue-500 font-bold text-lg">אישור</Text>
+            </TouchableOpacity>
+          </>
+        )}
       </View>
     </Modal>
   );
