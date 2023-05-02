22124fd90cea856924057a6be01bca3b30113b1d
Nadav Buchwalter <ndbuchwalter94@gmail.com>
2023-05-02 15:21:26 +0300

    Fixed Video Async bug

--- client/src/components/VideoBox.js
+++ client/src/components/VideoBox.js
@@ -1,21 +1,23 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { ResizeMode, Video } from 'expo-av';
 import React, { useState, useRef, useCallback } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 
-const VideoBox = ({uri}) => {
+const VideoBox = ({ uri }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const videoRef = useRef();
 
   useFocusEffect(
     useCallback(() => {
       return () => {
-        videoRef.current.pauseAsync();
+        if (videoRef.current) {
+          videoRef.current.pauseAsync();
+        }
         setIsPlaying(false);
       };
     }, [])
   );
-
+  
   return (
     <View className="items-center ">
       <Video
