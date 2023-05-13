4be15e367cb92ee38fc610b51acec38bb1eaf5ec
Nadav Buchwalter <ndbuchwalter94@gmail.com>
2023-05-13 20:12:53 +0300

    Updated mailer.js

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,13 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const mailer = nodemailer.createTransport({
+  pool: true,
   host: 'smtp-mail.outlook.com',
   auth: {
     user: process.env.EMAIL_AUTH_USER,
     pass: process.env.EMAIL_AUTH_PASSWORD,
   },
 });
-
-
 
 module.exports = mailer;
