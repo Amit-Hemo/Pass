412d22b1358f5518f43d612abf128862f00718ed
Amit Hemo <itsamit442@gmail.com>
2023-07-03 19:49:25 +0300

    Updated Screen UI

--- client/src/screens/ReleaseProductScreen.js
+++ client/src/screens/ReleaseProductScreen.js
@@ -1,27 +1,35 @@
-import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
-import { watchCart } from '../api/user';
-import useProductStore from '../stores/product';
+import { Dimensions, ScrollView, Text, View } from 'react-native';
+import ActionButton from '../components/ActionButton';
+import Box from '../components/Box';
+import Popup from '../components/Popup';
+import usePopup from '../hooks/usePopup';
 import useUserStore from '../stores/user';
-import countCartAmount from '../utils/countCartAmount';
-import filterUnavailableTags from '../utils/filterUnavailableTags';
 
 const ReleaseProductScreen = ({ navigation, route }) => {
-  const isScanned = useProductStore((state) => state.scanned);
-  const name = useProductStore((state) => state.name);
-  const size = useProductStore((state) => state.size);
   const uuid = useUserStore((state) => state.uuid);
-  const cart = route.params?.cart
+  const { modalVisible, setModalVisible, modalInfo, setModalInfo } = usePopup();
+  
 
-  //TODO: after finishing releasing, cart/scanned product must be removed!!!
+  const { height } = Dimensions.get('window');
+  const cart = route.params?.cart;
+  const singleProduct = route.params?.singleProduct
+
+
+  const clearProducts = () => {
+
+    //cart
+    
+      // clearCartMutation.mutate(uuid);
+    
+  };
 
   const productsToRelease = () => {
-    if (isScanned) {
+    if (singleProduct) {
       return (
         <View className='flex-row items-center justify-between'>
-          <Text className='text-xl pl-20 py-4'>{`${name} - ${size}`} </Text>
-          <Text className='text-xl font-bold text-stone-600'>1</Text>
+          <Text className='text-lg pl-20 py-1 font-bold'>{`${singleProduct?.name} - ${singleProduct?.size}`} </Text>
+          <Text className='text-lg font-bold text-stone-600'>1</Text>
         </View>
       );
     } else {
@@ -30,8 +38,8 @@ const ReleaseProductScreen = ({ navigation, route }) => {
           className='flex-row items-center justify-between'
           key={product.sku}
         >
-          <Text className='text-xl pl-20 py-4 font-bold'>{`${product.name} - ${product.size}`}</Text>
-          <Text className='text-xl font-bold text-stone-600'>
+          <Text className='text-lg pl-20 py-1 font-bold'>{`${product.name} - ${product.size}`}</Text>
+          <Text className='text-lg font-bold text-stone-600'>
             {tags.length}
           </Text>
         </View>
@@ -39,26 +47,39 @@ const ReleaseProductScreen = ({ navigation, route }) => {
     }
   };
 
-  let cartAmount = 0;
-  if (cart) {
-    cartAmount = countCartAmount(cart);
-  }
-
   return (
     <View className='items-center mt-10 px-7'>
-      <Text className='text-3xl'>תתחדשו!</Text>
-      <Text className='mt-5 mb-20 text-xl'>
-        הצמידו את הטלפון החכם לאטב לצורך שחרורו
+      <Text className='text-2xl'>תתחדשו!</Text>
+      <Text className='mt-5 mb-10 text-lg text-center'>
+        הצמידו את הטלפון החכם לאטב לצורך שחרור המוצרים:
       </Text>
 
-      <View className='border-2 rounded-lg h-3/6 items-center mb-2 px-4'>
-        <ScrollView>{productsToRelease()}</ScrollView>
-      </View>
+      <Box style={{ height: height / 3 + 30 }}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {productsToRelease()}
+        </ScrollView>
+      </Box>
 
-      {/* List len will be how many products , each release will reduce this number by 1 */}
-      <Text className='font-bold text-xl px-10'>
-        נותרו עוד {isScanned ? 1 : cartAmount} מוצרים לשחרור
+      <ActionButton
+        title='סיום'
+        handler={() => {
+          clearProducts();
+          navigation.goBack()
+          navigation.navigate('HomeScreen');
+        }}
+      />
+
+      <Text className='mt-5 text-center text-sm'>
+        במקרה של תהליך שחרור לא מוצלח, יש לפנות לעובד חנות
       </Text>
+
+      <Popup
+        visible={modalVisible}
+        isError={modalInfo.isError}
+        setVisible={setModalVisible}
+        onClose={modalInfo.onClose}
+        message={modalInfo.message}
+      />
     </View>
   );
 };
